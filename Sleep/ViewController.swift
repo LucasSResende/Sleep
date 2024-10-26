9c1e0a845ab8135adc9cca7b76e81deb24c159a6
LucasSResende <lucasresendesrs@gmail.com>
2024-10-26 15:35:25 -0400

    Initial Commit

---
+++ Sleep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sleep
+//
+//  Created by user269368 on 10/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SleepTests/SleepTests.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTests.swift
+//  SleepTests
+//
+//  Created by user269368 on 10/26/24.
+//
+
+import Testing
+@testable import Sleep
+
+struct SleepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
